57eb422911e24a58888c366001dde363508cbb5a
yusufstawan <yusufsetiyawan45@gmail.com>
2022-07-16 19:16:38 +0700

    Browser window
    Setting show to true to check showfile

--- main.js
+++ main.js
@@ -14,6 +14,33 @@ const menuItem = [
     label: 'File',
     submenu: [
       {
+        label: 'New Window html',
+        click: async () => {
+          // await shell.openExternal('https://electronjs.org')
+          const win2 = new BrowserWindow({
+            height: 300,
+            width: 400,
+            show: false,
+            backgroundColor: '#f0f0f0'
+          })
+
+          win2.loadFile('index2.html')
+        }
+      },
+      {
+        label: 'New Window url',
+        click: async () => {
+          // await shell.openExternal('https://electronjs.org')
+          const win2 = new BrowserWindow({
+            height: 300,
+            width: 400
+          })
+
+          win2.loadURL('https://kelasweb.id/')
+          win2.once('ready-to-show', () => win2.show())
+        }
+      },
+      {
         label: 'Learn More',
         click: async () => {
           await shell.openExternal('https://electronjs.org')
