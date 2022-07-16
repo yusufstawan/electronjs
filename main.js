a0645f99d552f9bf54421ea0e3b44fe663e908b8
yusufstawan <yusufsetiyawan45@gmail.com>
2022-07-16 17:00:50 +0700

    Learn ElectronJS First Commit

---
+++ main.js
@@ -0,0 +1,54 @@
+const { app, BrowserWindow, Menu } = require('electron')
+const path = require('path')
+
+const menuItem = [
+  {
+    label: 'About',
+    submenu: [
+      {
+        label: 'About',
+      }
+    ]
+  },
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Learn More',
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Exit',
+      }
+    ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(menuItem)
+Menu.setApplicationMenu(menu)
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    height: 500,
+    width: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
